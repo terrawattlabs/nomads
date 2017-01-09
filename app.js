a8f6a9e04ec395493eabd1d95e94dae92d663976
Jack <jack@Jacks-MacBook-Pro.local>
2017-01-09 16:31:49 -0700

    map

--- app.js
+++ app.js
@@ -164,6 +164,58 @@ app.get('/email', (req, res) => {
 
 });
 
+app.get('/map', (req, res) => {
+
+var gmAPI = new GoogleMapsAPI();
+var params = {
+  center: '444 W Main St Lock Haven PA',
+  zoom: 15,
+  size: '500x400',
+  maptype: 'roadmap',
+  markers: [
+    {
+      location: '300 W Main St Lock Haven, PA',
+      label   : 'A',
+      color   : 'green',
+      shadow  : true
+    },
+    {
+      location: '444 W Main St Lock Haven, PA',
+      icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_icon&chld=cafe%7C996600'
+    }
+  ],
+  style: [
+    {
+      feature: 'road',
+      element: 'all',
+      rules: {
+        hue: '0x00ff00'
+      }
+    }
+  ],
+  path: [
+    {
+      color: '0x0000ff',
+      weight: '5',
+      points: [
+        '41.139817,-77.454439',
+        '41.138621,-77.451596'
+      ]
+    }
+  ]
+};
+gmAPI.staticMap(params); // return static map URL
+gmAPI.staticMap(params, function(err, binaryImage) {
+  console.log(binaryImage);
+  res.send(binaryImage);
+
+  // fetch asynchronously the binary image
+});
+
+  
+
+});
+
 
 // Main page of app with link to log in
 app.get('/', (req, res) => {
